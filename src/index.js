72f6695440c888420791f7301e15254132022d44
audetv <audetv@gmail.com>
2021-02-19 17:59:59 +0300

    Changed the App using react jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return React.createElement('div', {id: 'test-id'}, 'Hello from React.');
+    return <div id="test-id">Hello from React.</div>;
 }
 
 ReactDOM.render(React.createElement(App), document.querySelector('#root'));
