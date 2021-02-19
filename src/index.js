826c420041fd3e22adaf33dce3c5da031788cdd0
audetv <audetv@gmail.com>
2021-02-19 17:59:19 +0300

    Added simple App using react createElement

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return React.createElement('div', {id: 'test-id'}, 'Hello from React.');
+}
+
+ReactDOM.render(React.createElement(App), document.querySelector('#root'));
