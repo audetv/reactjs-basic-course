6b2ea86b4e81ba29f7890b5ada5498d2d4731ff6
audetv <audetv@gmail.com>
2021-02-20 00:12:17 +0300

    Configured babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'static', 'build'),
-    filename: "bundle.js",
-    publicPath: "/static/build/",
-  }
-}
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'static', 'build'),
+        filename: "bundle.js",
+        publicPath: "/static/build/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/env', '@babel/preset-react']
+                },
+            },
+        ],
+    },
+};
