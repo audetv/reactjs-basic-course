898f016844605fe8dc614a69b2d22796ccf79215
audetv <audetv@gmail.com>
2021-03-04 10:50:25 +0300

    removed test forms

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,10 @@
 import {Messages} from "../Messages";
 import './App.css'
-import {Form} from "../Form/Form";
 
 const App = () => {
     return <div id="test-id">
         <h2 className="title">Hello from React.</h2>
-        {/*<Messages />*/}
-        <Form />
+        <Messages />
     </div>;
 };
 
