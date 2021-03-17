254b7507a7fd5dea90d27f39d83943995e74430e
audetv <audetv@gmail.com>
2021-03-17 14:50:53 +0300

    Refactored header for hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
-import { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { withStyles, AppBar, Typography, Toolbar, Button, IconButton } from '@material-ui/core';
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Button,
+  IconButton,
+  makeStyles,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const useStyles = theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -15,29 +21,26 @@ const useStyles = theme => ({
     color: '#fff',
     textDecoration: 'none',
   },
-});
+}));
 
-class _Header extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title} component={Link} to={'/'}>
-              Messenger
-            </Typography>
-            <Button color="inherit" component={Link} to='/profile'>Profile</Button>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title} component={Link} to={'/'}>
+            Messenger
+          </Typography>
+          <Button color="inherit" component={Link} to='/profile'>Profile</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-const Header = withStyles(useStyles)(_Header);
 export { Header }
